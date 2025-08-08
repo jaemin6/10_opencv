d337195e0eda67f8559c7f55fe9d64aff70f178c
jaemin6 <gumt555@gmail.com>
2025-08-08 10:32:47 +0900

    수정

--- src/ocr.py
+++ src/ocr.py
@@ -3,9 +3,9 @@ import cv2
 import matplotlib.pyplot as plt
 
 reader = easyocr.Reader(['ch_tra', 'en'], gpu=False)
-result = reader.readtext('ch_tr.jpg')
+# result = reader.readtext('ch_tr.jpg')
 
-img = cv2.imread('ch_tr.jpg')
+img = cv2.imread('../img/ch_tr.jpg')
 result = reader.readtext(img)
 
 plt.figure(figsize=(8, 8))
